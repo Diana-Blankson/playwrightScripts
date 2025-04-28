b9bee18dd5ea896915fed053158aa9122baa5ca7
Diana Blankson <dblankson.ext@igus.net>
2025-04-28 16:46:10 +0000

    Rectify issue with naming convention

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -12,13 +12,13 @@ test.describe('Login Scenarios', () => {
 
   test('Valid Users', async ({ page }) => {
     
-    await login.loginPage(data.validUser.username, data.password);
+    await login.loginPage(data.validUser.userName, data.password);
     await expect(page).toHaveURL(/inventory/);
   });
 
   test('Locked out users', async ({ page }) => {
 
-    await login.loginPage(data.lockedOutUser.username, data.password);
+    await login.loginPage(data.lockedOutUser.userName, data.password);
     await expect(page.locator('[data-test="error"]')).toHaveText(
       'Epic sadface: Sorry, this user has been locked out.'
     );
